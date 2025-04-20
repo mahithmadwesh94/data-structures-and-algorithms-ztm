f775d925de7c2f7eabb059130352b12ac163ea0d
mahith.vasisht <mahith.madwesh@gmail.com>
2025-04-20 11:08:09 +0530

    linkedList JS methods

---
+++ linkedList/linkedlist.js
@@ -0,0 +1,156 @@
+// prepend, append O(n)
+// lookup, insert, delete O(n)
+
+
+// Alternatively you can write node as class as well
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    newNode(value) {
+        return {
+            value: value,
+            next: null
+        }
+    }
+
+    append(value) {
+        let node = this.newNode(value)
+
+        this.tail.next = node;
+        this.tail = node;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        let node = this.newNode(value)
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+        return this;
+    }
+
+    printList() {
+        let array = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+        let i = 0;
+        let currentNode = this.head;
+        let node = this.newNode(value);
+        if (index == 0) {
+            this.prepend(value);
+            // this.length++;
+            return this.printList();
+        }
+
+        if (index > this.length - 1) {
+            this.append(value);
+            // this.length++;
+            return this.printList();
+        }
+
+
+        while (i != index - 1) {
+            if (currentNode.next != null) {
+                currentNode = currentNode.next;
+                i++;
+            }
+
+        }
+
+        node.next = currentNode.next;
+        currentNode.next = node;
+        this.length++;
+        return this.printList();
+
+
+    }
+     remove(index){
+        //if first index
+        if(index === 0){
+            let newHead = this.head.next;
+            let removeNode = this.head;
+            removeNode.next = null;
+            this.head = newHead;
+            this.length--;
+            return removeNode;
+            
+            
+            
+        }
+
+         if(index > this.length-1){
+             return `Please enter an index between 0 and ${this.length - 1 }`
+         }
+
+         let i = 0;
+         let currentNode = this.head;
+         while(i != index - 1){
+            
+             if(currentNode.next != null){
+                 currentNode = currentNode.next;
+                 i++;
+             }
+         }
+         
+   
+         let removeNode = currentNode.next;
+         if(removeNode.next == null){
+             this.tail = currentNode;
+             this.tail.next = null;
+             
+         }else{
+             let newNode = removeNode.next;
+              currentNode.next = newNode;
+         }
+         
+         this.length--;
+        
+         removeNode.next=null;
+         
+         return removeNode;
+         
+    }
+}
+
+
+const myList = new LinkedList(10);
+// console.log(myList);
+
+myList.append(5); //O(1)
+myList.append(16) //O(1)
+myList.prepend(1) //O(1)
+// myList.printList();
+myList.insert(200, 99);
+myList.insert(0, 100);
+myList.insert(3, 3);
+myList.printList();
+myList.remove(6);
+console.log(myList.length);
+myList.printList();
+myList.remove(6);
+
+
+
