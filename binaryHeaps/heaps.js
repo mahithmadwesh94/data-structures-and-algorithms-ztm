ac7622c658829a4bc74ee8b01b683093fe9d1386
mahith.vasisht <mahith.madwesh@gmail.com>
2025-04-27 09:00:59 +0530

    Heaps, Trie, Priority Queues

---
+++ binaryHeaps/heaps.js
@@ -0,0 +1,8 @@
+// Every child belongs to a parent who has a higher priority or value
+// max heap => where parent node is highest compared to children
+//  min heap => where parent node is the least compared to children
+// lookup O(n)
+// insert and delete O(log N)
+// left to right and then bubbles up
+// priority queues
+// Trie - O(length of the word). Good with space complexity
